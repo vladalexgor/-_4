caf8938aabfe8a68c262a43b27d510e883c24bf0
vladalexgor@ya.ru <vladalexgor@ya.ru>
2021-11-23 00:29:45 +0300

    Добавьте файлы проекта.

---
+++ Задание_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4
+{
+    class Program
+    {
+        //Ввести целое число N > 0. Найти квадрат данного числа, используя для его вычисления следующую формулу: N2=1 + 3 + 5 + ... + (2*N – 1).
+        //После добавления к сумме каждого слагаемого выводить текущее значение суммы (в результате будут выведены квадраты всех целых чисел от 1 до N).
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВедите целое число N > 0.");
+            int N = Convert.ToInt32(Console.ReadLine());
+            //Sum - текущее значение суммы.
+            int Sum = 0;
+            int i;
+            for (i = 1; (i <= N); i++)
+            {
+                Sum = Sum + (2 * i - 1);
+                Console.WriteLine("Текущее значение суммы: Sum = {0}", Sum);
+            }
+            Console.WriteLine("Квадрат числа N = {0}", Sum);
+            Console.WriteLine("Для завершения программы нажмите любую кнопку на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание_4_App2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4_App2
+{
+    class Program
+    {
+        //Осуществить ввод последовательности целых чисел и сравнить, что больше, количество положительных или количество отрицательных.
+        //Последовательность потенциально не ограничена, окончанием последовательности служит число 0
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность целых чисел в любом количестве, в конце последовательности поставьте 0:");
+            int positive = 0;
+            int negative = 0;
+            int N = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                if (N > 0)
+                {
+                    positive++;
+                }
+                else if (N < 0)
+                {
+                    negative++;
+                }
+                N = Convert.ToInt32(Console.ReadLine());
+            } while (N != 0);
+            if (positive > negative)
+            {
+                Console.WriteLine("Положительных чисел больше отрицательных.");
+            }
+            else if (positive < negative)
+            {
+                Console.WriteLine("Отрицательных чисел больше положительных.");
+            }
+            else
+            {
+                Console.WriteLine("Количество положительных и отрицательных чисел равно.");
+            }
+            Console.WriteLine("Для завершения программы нажмите любую клавишу на клавиатуре.");
+            Console.ReadKey();
+        }
+    }
+}
